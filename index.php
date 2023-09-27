c5ca48694664627654d682f34f085d8986b63a77
RazerKriht <55840288+RazerKriht@users.noreply.github.com>
2023-09-27 07:48:27 +0300

    1
    1

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Шаг вперёд</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body style="background-color:#3eb489">
+    <header>
+        <a href="index.php"><img src="logo.png" alt=""></a>
+        <h1>Рейтинг клуба по северной ходьбе "ШАГ ВПЕРЁД"</h1>
+    </header>
+    <br />
+    <br />
+    <br />
+    <a><b>Регистрация</b></a>
+    <br />
+    <a href="rules.php"><b>Правила</b></a>
+<table>
+<tr><th><b>ФИО:</b></th><th><b>Фото:</b></th><th><b>Дата рождения:</b></th><th><b>Город:</b></th><th><b>Колличество баллов:</b></th></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+<tr><td>данные</td><td>данные</td><td>данные</td><td>данные</td><td>данные</td></tr>
+</table>
+
+</body>
+</html>
---
+++ rules.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Правила</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body style="background-color:#3eb489">
+    <header>
+        <a href="index.php"><img src="logo.png" alt=""></a>
+        <h1>Рейтинг клуба по северной ходьбе "ШАГ ВПЕРЁД"</h1>
+    </header>
+   
+
+</body>
+</html>
